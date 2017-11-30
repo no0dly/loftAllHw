7cee469f7fcbf5a9d93d782c560347cbe612ec7d
Vadim Derman <thenoodly@gmail.com>
2017-11-30 00:35:19 -0800

    fixed tests

--- facegit/src/sagas/__tests__/users.test.js
+++ facegit/src/sagas/__tests__/users.test.js
@@ -1,39 +1,39 @@
 import {
   fetchUserDataSuccess,
-  fetchUserDataFailure
+  fetchUserDataFailure,
+  clearUserData
 } from "../../actions/users";
+import { fetchFollowersRequest } from "../../actions/followers";
 import { call, put } from "redux-saga/effects";
 import { fetchUserDataRequestSaga } from "../users";
 import { getUserInformation } from "../../api";
 
 describe("Saga users:", () => {
+  const action = {
+    payload: "test_login"
+  };
+  const user = {
+    data: {
+      login: "test",
+      id: "1"
+    }
+  };
+  const saga = fetchUserDataRequestSaga(action);
   it("call getUserInformation", () => {
-    const action = {
-      payload: "test_login"
-    };
-    const saga = fetchUserDataRequestSaga(action);
     expect(saga.next().value).toEqual(call(getUserInformation, "test_login"));
   });
+  it("dispatch actions after success call (clearUserData)", () => {
+    expect(saga.next(user).value).toEqual(put(clearUserData()));
+  });
   it("dispatch action fetchUserDataSuccess with user from call on success call", () => {
-    const action = {
-      payload: "test_login"
-    };
-    const user = {
-      data: {
-        login: "test",
-        id: "1"
-      }
-    };
-    const saga = fetchUserDataRequestSaga(action);
-    saga.next();
-    saga.next();
-    console.log(saga.next(user.data).value);
-    expect(saga.next(user).value).toEqual(put(fetchUserDataSuccess(user)));
+    expect(saga.next().value).toEqual(put(fetchUserDataSuccess(user.data)));
+  });
+  it("dispatch action fetchFollowersRequest", () => {
+    expect(saga.next().value).toEqual(
+      put(fetchFollowersRequest(action.payload))
+    );
   });
   it("dispatch action fetchUserDataFailure with user from call on success call", () => {
-    const action = {
-      payload: "test_login"
-    };
     const error = new Error("test error");
     const saga = fetchUserDataRequestSaga(action);
     saga.next();
